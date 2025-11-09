f7ecb5dac8022525b62f0e18ffdaedff504ad6ec
aticcmahbub <aticcmahbub@gmail.com>
2025-11-09 11:34:05 +0600

    ui: create dashboard layout

---
+++ src/pages/user/UserBookings.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function UserBookings() {
+    return <div>UserBookings</div>;
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,13 @@
 import App from '@/App';
-import AdminLayout from '@/layouts/AdminLayout';
-import DriverLayout from '@/layouts/DriverLayout';
-import RiderLayout from '@/layouts/RiderLayout';
 import About from '@/pages/public/About';
 import LoginPage from '@/pages/auth/LoginPage';
 import RegistrationPage from '@/pages/user/RegistrationPage';
 import VerifyPage from '@/pages/otp/VerifyOTPPage';
 
 import {createBrowserRouter} from 'react-router';
+import DashboardLayout from '@/layouts/DashboardLayout';
+import Analytics from '@/pages/Analytics';
+import UserBookings from '@/pages/user/UserBookings';
 
 export const router = createBrowserRouter([
     {
@@ -16,11 +16,23 @@ export const router = createBrowserRouter([
         children: [{path: 'about', Component: About}],
     },
 
-    {path: 'login', Component: LoginPage},
+    {
+        path: '/admin',
+        Component: DashboardLayout,
+        children: [{path: 'analytics', Component: Analytics}],
+    },
+
+    {
+        path: '/user',
+        Component: DashboardLayout,
+        children: [{path: 'bookings', Component: UserBookings}],
+    },
+
+    {path: '/login', Component: LoginPage},
     {path: '/registration', Component: RegistrationPage},
     {path: '/verify', Component: VerifyPage},
 
-    {path: 'admin', Component: AdminLayout},
-    {path: 'rider', Component: RiderLayout},
-    {path: 'driver', Component: DriverLayout},
+    // {path: '/admin', Component: AdminLayout},
+    // {path: '/rider', Component: RiderLayout},
+    // {path: '/driver', Component: DriverLayout},
 ]);
